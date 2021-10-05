b1aad3c618e6fccee011ece056dd2fbfeb25c661
David Carlos <ddavidcarlos1392@gmail.com>
2021-10-05 19:36:54 -0300

    Fixes for django 3

--- src/boogie/utils/random_names.py
+++ src/boogie/utils/random_names.py
@@ -1,7 +1,7 @@
 from random import choice
 
 from django.conf import settings
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 NAME_FORMAT = getattr(settings, "RANDOM_NAME_FORMAT", _("{adjective} {noun}"))
 
